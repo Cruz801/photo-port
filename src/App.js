4496c4ac34c2f12208336456de743dcac58f3c4e
Steven Cruz <cruz801@hotmail.com>
2022-03-14 01:04:33 -0600

    contact

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
-// import PhotoList from './components/PhotoList';
+import ContactForm from './components/Contact';
 
 
 function App() {
   const [categories] = useState([
+   
     {
       name: 'commercial',
       description: 'Photos of grocery stores, food trucks, and other commercial projects',
@@ -15,7 +16,7 @@ function App() {
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
-
+  const [contactSelected, setContactSelected] = useState(false);
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
   return (
     <div>
@@ -23,10 +24,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-      <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
+      {!contactSelected ? (
+        <>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
+        </>
+      ) : (
+        <ContactForm></ContactForm>
+      )}
       </main>
     </div>
   )
